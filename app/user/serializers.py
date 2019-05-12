103d444096685f43f10ecf41d9b737fc28771f56
Grzegorz Goryszewski <greg.szewski@gmail.com>
2019-05-12 10:14:30 +0200

    Original exception text was: 'AnonymousUser' object has no attribute 'email'.

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -16,6 +16,17 @@ class UserSerializer(serializers.ModelSerializer):
         """Create a new user with encrypted password and return it"""
         return get_user_model().objects.create_user(**validated_data)
 
+    def update(self, instance, validated_data):
+        """update user , setting the pass corectly and return it"""
+        password = validated_data.pop('password', None)
+        user = super().update(instance, validated_data)
+
+        if password:
+            user.set_password(password)
+            user.save()
+        
+        return user
+
 
 class AuthTokenSerializer(serializers.Serializer):
     """Serialize for user auth object"""
